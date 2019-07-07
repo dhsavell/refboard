8d8dde7eb43e1ada38480795a6e0ddc0ff55c62b
dhsavell <dhsavell05@gmail.com>
2019-07-07 18:24:47 -0400

    Implement basis of free transform

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,60 @@
-use yew::events::IMouseEvent;
-use yew::services::ConsoleService;
+use std::cmp;
+use std::f64::consts::FRAC_PI_4;
+use stdweb::traits::IMouseEvent;
 use yew::{html, Component, ComponentLink, Html, Renderable, ShouldRender};
 
+/// A Card is a transformable image displayed on the Refboard canvas.
 #[derive(PartialEq)]
 pub struct Card {
+    /// The image displayed by this card. This value is directly used in HTML
+    /// `img` tags.
     image: String,
+
+    /// The absolute position of the top-left corner of this card, represented
+    /// as an (x, y) tuple.
     position: (i32, i32),
+
+    /// The absolute size of this card, represented as a (width, height) tuple.
+    size: (i32, i32),
+
+    /// The rotation of this card in degrees.
+    rotation: f64,
 }
 
+/// A Model represents the state of the webapp.
 pub struct Model {
-    console: ConsoleService,
+    /// A vector of all cards on the Refboard canvas.
     cards: Vec<Card>,
-    drag_idx: Option<usize>,
+
+    /// The current action bound to mouse movement.
+    drag_state: DragState,
 }
 
+/// A DragState represents an action controlled by holding down the left mouse
+/// button and moving the mouse.
+pub enum DragState {
+    /// Mouse movement should be ignored.
+    None,
+
+    /// The card with the given index should be moved with the cursor.
+    MoveCard(usize),
+
+    /// The card with the given index should be scaled from the bottom-right.
+    MoveScaleHandle(usize),
+
+    /// The card with the given index should be rotated about its center.
+    MoveRotateHandle(usize),
+}
+
+/// A Msg (Message) is a signal sent to the Model requesting a controlled state
+/// change.
 pub enum Msg {
     CreateCard(String, (i32, i32)),
     RemoveCard(u32),
-    StartDragging(usize),
-    Drag((i32, i32)),
+    StartDraggingScaleHandle(usize),
+    StartDraggingRotateHandle(usize),
+    StartDraggingCard(usize),
+    Drag((i32, i32), (i32, i32)),
     StopDragging,
 }
 
@@ -28,37 +64,67 @@ impl Component for Model {
 
     fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
         Model {
-            console: ConsoleService::new(),
             cards: vec![
                 Card {
                     image: "".to_string(),
                     position: (0, 0),
+                    size: (300, 300),
+                    rotation: 0.0,
                 },
                 Card {
                     image: "".to_string(),
                     position: (400, 0),
+                    size: (300, 300),
+                    rotation: 0.0,
                 },
             ],
-            drag_idx: Option::None,
+            drag_state: DragState::None,
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::StartDragging(idx) => {
-                self.drag_idx = Option::Some(idx);
+            Msg::StartDraggingCard(idx) => {
+                self.drag_state = DragState::MoveCard(idx);
                 true
             }
-            Msg::Drag(delta) => match self.drag_idx {
-                Some(idx) => {
+            Msg::StartDraggingScaleHandle(idx) => {
+                self.drag_state = DragState::MoveScaleHandle(idx);
+                true
+            }
+            Msg::StartDraggingRotateHandle(idx) => {
+                self.drag_state = DragState::MoveRotateHandle(idx);
+                true
+            }
+            Msg::Drag(delta, pos) => match self.drag_state {
+                DragState::MoveCard(idx) => {
                     self.cards[idx].position.0 += delta.0;
                     self.cards[idx].position.1 += delta.1;
                     true
                 }
-                None => false,
+                DragState::MoveScaleHandle(idx) => {
+                    // TODO: Handle rotation
+                    self.cards[idx].size.0 = cmp::max(0, self.cards[idx].size.0 + delta.0);
+                    self.cards[idx].size.1 = cmp::max(0, self.cards[idx].size.1 + delta.1);
+                    true
+                }
+                DragState::MoveRotateHandle(idx) => {
+                    let card = &self.cards[idx];
+                    let (cursor_x, cursor_y) = pos;
+                    let (x, y) = card.position;
+                    let (width, height) = card.size;
+
+                    let atan_x: f64 = (cursor_x - (x + (width / 2))).into();
+                    let atan_y: f64 = (cursor_y - (y + (height / 2))).into();
+
+                    self.cards[idx].rotation = atan_y.atan2(atan_x) + FRAC_PI_4;
+
+                    true
+                }
+                DragState::None => false,
             },
             Msg::StopDragging => {
-                self.drag_idx = Option::None;
+                self.drag_state = DragState::None;
                 true
             }
             _ => true,
@@ -70,7 +136,7 @@ impl Renderable<Model> for Model {
     fn view(&self) -> Html<Self> {
         html! {
             <div class="refboard",
-                    onmousemove=|e| Msg::Drag((e.movement_x(), e.movement_y())),
+                    onmousemove=|e| Msg::Drag((e.movement_x(), e.movement_y()), (e.client_x(), e.client_y())),
                     onmouseup=|_| Msg::StopDragging,>
                 { for self.cards.iter().map(|c| self.view_card(c)) }
             </div>
@@ -85,9 +151,23 @@ impl Model {
         match card_idx {
             Some(idx) => html! {
                 <div class="unselectable card",
-                        style=format!("left: {}px; top: {}px;", card.position.0, card.position.1),
-                        onmousedown=|_| Msg::StartDragging(idx),>
-                    <div class="image",></div>
+                        style=format!("left: {}px; top: {}px; width: {}px; height: {}px; transform: rotate({}rad)", card.position.0, card.position.1, card.size.0, card.size.1, card.rotation),>
+
+                    // Transformation handles
+
+                    <div class="scaling-handle",
+                        style="right: -5px; bottom: -5px;",
+                        onmousedown=|_| Msg::StartDraggingScaleHandle(idx),></div>
+
+                    <div class="rotation-handle",
+                        style="right: -5px; top: -5px;",
+                        onmousedown=|_| Msg::StartDraggingRotateHandle(idx),></div>
+
+                    // Actual image body
+
+                    <div class="image",
+                        onmousedown=|_| Msg::StartDraggingCard(idx),
+                        style=format!("width: {}px; height: {}px", card.size.0, card.size.1),></div>
                 </div>
             },
             None => html! {},
