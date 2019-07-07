3e7267af718a306cb780d363690e88cd97f1584d
dhsavell <dhsavell05@gmail.com>
2019-07-07 14:29:03 -0400

    Allow multiple cards to be manipulated

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,10 @@ use yew::events::IMouseEvent;
 use yew::services::ConsoleService;
 use yew::{html, Component, ComponentLink, Html, Renderable, ShouldRender};
 
-type Degrees = f64;
-
-pub enum Scale {
-    Proportional(f64),
-    Independent(f64, f64),
-}
-
+#[derive(PartialEq)]
 pub struct Card {
     image: String,
     position: (i32, i32),
-    scale: Scale,
-    rotation: Degrees,
 }
 
 pub struct Model {
@@ -37,12 +29,16 @@ impl Component for Model {
     fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
         Model {
             console: ConsoleService::new(),
-            cards: vec![Card {
-                image: "".to_string(),
-                position: (0, 0),
-                rotation: 0.0,
-                scale: Scale::Proportional(1.0),
-            }],
+            cards: vec![
+                Card {
+                    image: "".to_string(),
+                    position: (0, 0),
+                },
+                Card {
+                    image: "".to_string(),
+                    position: (400, 0),
+                },
+            ],
             drag_idx: Option::None,
         }
     }
@@ -70,24 +66,31 @@ impl Component for Model {
     }
 }
 
-fn view_card(card: &Card) -> Html<Model> {
-    html! {
-        <div class="unselectable card",
-                style=format!("left: {}px; top: {}px;", card.position.0, card.position.1),
-                onmousedown=|_| Msg::StartDragging(0),>
-            <div class="image",></div>
-        </div>
-    }
-}
-
 impl Renderable<Model> for Model {
     fn view(&self) -> Html<Self> {
         html! {
             <div class="refboard",
-                onmousemove=|e| Msg::Drag((e.movement_x(), e.movement_y())),
-                onmouseup=|_| Msg::StopDragging,>
-                { for self.cards.iter().map(view_card) }
+                    onmousemove=|e| Msg::Drag((e.movement_x(), e.movement_y())),
+                    onmouseup=|_| Msg::StopDragging,>
+                { for self.cards.iter().map(|c| self.view_card(c)) }
             </div>
+        }
+    }
+}
+
+impl Model {
+    fn view_card(&self, card: &Card) -> Html<Model> {
+        let card_idx = self.cards.iter().position(|c| c == card);
+
+        match card_idx {
+            Some(idx) => html! {
+                <div class="unselectable card",
+                        style=format!("left: {}px; top: {}px;", card.position.0, card.position.1),
+                        onmousedown=|_| Msg::StartDragging(idx),>
+                    <div class="image",></div>
+                </div>
+            },
+            None => html! {},
         }
     }
 }
